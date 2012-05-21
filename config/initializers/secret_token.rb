747a3df0fc16dadc92c5417ba376dfcd91e20bb7
Graham Gaylor <graham.gaylor@gmail.com>
2012-05-21 09:58:23 -0500

    Created initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VandyMobileWeb::Application.config.secret_token = '9ec69a55f4de3672f2433193378a280f3b01e849837b79b5cbb094d94a0107963984883ea01ba905a84586af6226ae8bd8ebaf036f3abee5ea1679148fa5ec6d'
