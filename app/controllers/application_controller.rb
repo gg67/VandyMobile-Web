d5a38491b15f8b74abf6e28794f5816dd5d5b936
gg67 <graham.gaylor@chaione.com>
2012-08-29 08:31:54 -0500

    Added site lock feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   include ControllerAuthentication
   protect_from_forgery
+  
+  before_filter :authenticate
+
+	protected
+	
+	def authenticate
+	  authenticate_or_request_with_http_basic do |username, password|
+	    username == "foo" && password == "bar"
+	  end
+	end
 end
